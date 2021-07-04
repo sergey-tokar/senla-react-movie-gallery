91206350c984f2c6b8c081f7aae26cd039ba119a
Sergey Tokar <tokarsergey1988@gmail.com>
2021-07-04 17:52:10 +0300

    feat: add footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import React from 'react';
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
     return (
-        <Header/>
+        <div>
+            <Header/>
+            <Footer/>
+        </div>
+
     );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <div className="content-wrapper">
+                <p className="footer-info">SENLA© FRONTEND COURSES: final React project</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
